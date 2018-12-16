967d84c962cac950a53897c0cf21760a31f99439
sijincao <1240182843@qq.com>
2018-12-16 09:59:23 +0800

    init

---
+++ mooc/src/main/java/com/mooc/MoocApplication.java
@@ -0,0 +1,14 @@
+package com.mooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoocApplication.class, args);
+    }
+
+}
+
